f15cb8f0bc5f7fae76ab5be0502f0f1d4ebfaf99
jata-keita <jatakeita40@gmail.com>
2021-10-11 05:04:38 -0700

    first commit

---
+++ ndex.py
@@ -0,0 +1,54 @@
+# x = int(input("enter your mittrm"))
+# y = int(input('enter your test'))
+# z= int(input("enter your final exam"))
+
+# h = (x + y + z) / 200 *100
+# print(h)
+
+
+# valu1 = int(input("enter your first value "))
+# valu2 = int(input("enter your second value "))
+
+# mum_total = valu1 * valu2
+# print(f"the first {valu1} and the second {valu2} is {mum_total}")
+
+
+
+
+# x= 4
+# print(x)
+
+# assigment 1
+
+# dalase = eval(input("enter your money in dalasis "))
+
+# Euro = 59.6205
+# Doler = 51.50
+
+# convert1 = dalase * Euro
+# convert2 = dalase * Doler
+
+# print(f'Your money in Euros is {convert1}€ and in dollars {convert2}$')
+
+#    input pay
+#    input rent
+#    input utilities
+#    input groceries
+#    set discretionary = pay - rent - utlilities - groceries
+#    output discretionary
+
+#assigment 2
+
+
+# pay = eval(input("enter your payment "))
+# rent = eval(input("enter your rent "))
+# utilities = eval(input("enter your utilities "))
+# graceries = eval(input("enter your graceries "))
+
+# discretionary = pay - rent - utilities - graceries
+# print(f'This is your final discretionary {discretionary}') 
+
+
+
+
+
